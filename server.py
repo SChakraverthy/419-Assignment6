720fe455911e1e078cc8b6b2fc6be28dc30e3f28
shikhanair <shikhavnair@gmail.com>
2018-04-27 15:37:19 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 import socket
@@ -9,11 +8,24 @@ import hashlib
 import ssl
 import pickle
 import re
+import time
+import datetime
 
 
 #Turns Message Board name in a file-friendly name for the Message Board file
 def fileFriendly(name):
     return re.sub(r'\W+', '', name)
+
+
+def post(grpname, msg):
+    
+    grp = fileFriendly(grpname)
+    
+    with open(grp+".p", "wb") as mb:
+        nmsg = msg + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        pickle.dump(nmsg, mb)
+    
+    return 
 
 
 #this function takes the username and password provided by the client and verifies the login information
@@ -60,6 +72,52 @@ def handleconn(conn):
 		elif(cmd =="END"):
 			print("Received END command from client!")
 			break
+		else:
+			print("No valid commands received")
+
+
+
+
+def main():
+	port = 5100
+	
+	# Create the SSL Context
+	context = ssl.SSLContext()
+	context.check_hostname=False
+	context.load_cert_chain(certfile="certfile.crt", keyfile="keyfile.key")
+
+	#Create the socket and bind it to the defined port.
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('', port))
+
+	#Listen for connections
+	s.listen(10)
+	print('Listening for connections...')
+
+	while True:
+		
+		# Accept incoming connections
+		(c, addr) = s.accept()
+		print('Connection accepted from: ', addr)
+
+		# Wrap the normal socket.
+		try:
+			conn = context.wrap_socket(c, server_side=True)
+		except ssl.SSLError as e:
+			print("wrap_socket Error: ", e)
+			s.close()
+			c.close()
+			return
+
+		handleconn(conn)
+
+		c.close()
+		break
+
+	s.close()
+
+if __name__ == "__main__":
+	main()break
 		else:
 			print("No valid commands received")
 
